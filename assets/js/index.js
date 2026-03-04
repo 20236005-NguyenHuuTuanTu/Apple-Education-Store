9b05af02901e87b32f6fae94332b60e66c572214
20235843-thuynt <nguyenthanhthuy30092005@gmail.com>
2026-03-04 12:00:27 +0700

    feat(ui): add all product sections

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,35 +1,37 @@
 document.addEventListener('DOMContentLoaded', function () {
-	const wrapper = document.querySelector('.card-wrapper');
-	const prevBtn = document.querySelector('.slider-btn.prev');
-	const nextBtn = document.querySelector('.slider-btn.next');
+    const wrappers = document.querySelectorAll('.card-wrapper');
+    if (!wrappers) return;
 
-	if (!wrapper || !prevBtn || !nextBtn) return;
+    wrappers.forEach((wrapper) => {
+        const prevBtn = wrapper.parentElement.querySelector('.slider-btn.prev');
+        const nextBtn = wrapper.parentElement.querySelector('.slider-btn.next')   
+        if (!prevBtn || !nextBtn) return;
+        
+        function getCardGap() {
+            const gap = getComputedStyle(wrapper).gap || getComputedStyle(wrapper).columnGap;
+            return parseInt(gap, 10) || 0;
+        }
 
-	function getCardGap() {
-		const gap = getComputedStyle(wrapper).gap || getComputedStyle(wrapper).columnGap;
-		return parseInt(gap, 10) || 0;
-	}
+        function scrollByCard(dir = 1) {
+            const firstCard = wrapper.querySelector('.card');
+            if (!firstCard) return;
+            const cardWidth = firstCard.getBoundingClientRect().width;
+            const gap = getCardGap();
+            const amount = (cardWidth + gap) * dir;
+            wrapper.scrollBy({ left: amount, behavior: 'smooth' });
+        }
 
-	function scrollByCard(dir = 1) {
-		const firstCard = wrapper.querySelector('.card');
-		if (!firstCard) return;
-		const cardWidth = firstCard.getBoundingClientRect().width;
-		const gap = getCardGap();
-		const amount = (cardWidth + gap) * dir;
-		wrapper.scrollBy({ left: amount, behavior: 'smooth' });
-	}
+        // enable/disable at edges
+        function updateButtons() {
+            const max = wrapper.scrollWidth - wrapper.clientWidth;
+            prevBtn.disabled = wrapper.scrollLeft <= 0;
+            nextBtn.disabled = wrapper.scrollLeft >= max - 1;
+        }
 
-	prevBtn.addEventListener('click', () => scrollByCard(-1));
-	nextBtn.addEventListener('click', () => scrollByCard(1));
-
-	// enable/disable at edges
-	function updateButtons() {
-		const max = wrapper.scrollWidth - wrapper.clientWidth;
-		prevBtn.disabled = wrapper.scrollLeft <= 0;
-		nextBtn.disabled = wrapper.scrollLeft >= Math.round(max - 1);
-	}
-
-	wrapper.addEventListener('scroll', updateButtons);
-	window.addEventListener('resize', updateButtons);
-	updateButtons();
+        prevBtn.addEventListener('click', () => scrollByCard(-1));
+        nextBtn.addEventListener('click', () => scrollByCard(1));
+        wrapper.addEventListener('scroll', updateButtons);
+        window.addEventListener('resize', updateButtons);
+        updateButtons();
+    });
 });
